1775d839ca32434a766d61a537f67af20dc65d04
Siponek <szinkowicz@gmail.com>
2022-02-23 15:59:55 +0000

    TODO assistance algorythm

--- assigment_rt_3/scripts/UI_Robot.py
+++ assigment_rt_3/scripts/UI_Robot.py
@@ -21,13 +21,26 @@ menu_options = {
     2: 'Drive the robot with the keyboard',
     3: 'Drive the robot with assistance to avoid collisions',
     4: 'Exit',
-    5: 'TIME TO STOP',
+    5: "IT'S TIME TO STOP",
 }
 
 
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+
 def print_menu():
+    print("\033[96m")
+
     for key in menu_options.keys():
         print(key, '--', menu_options[key])
+    print("\033[00m")
 
 
 def option1():
@@ -86,8 +99,8 @@ if __name__ == '__main__':
     pubSignalAssisted = rospy.Publisher(
         "assistedDriveCheck", std_msgs.msg.Bool, queue_size=1)
 
+    print_menu()
     while(True):
-        print_menu()
         option = None
         try:
             option = int(input('Enter your choice: '))
@@ -102,7 +115,7 @@ if __name__ == '__main__':
             option3()
         elif option == 4:
             print('Thanks message before exiting')
-            # TODO rospy.shutdown()
+            # TODO rospy.shutdown() on other nodes as well
             # rospy.signal_shutdown("User choice (4)")
             # rospy.spin()
             exit()
@@ -110,4 +123,5 @@ if __name__ == '__main__':
             option5()
 
         else:
+
             print('Invalid option. Please enter a number between 1 and 4.')
--- assigment_rt_3/scripts/master_Robot.py
+++ assigment_rt_3/scripts/master_Robot.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
-from platform import system
 import rospy
 # # *for string, bool, empty
 from std_msgs.msg import Float32MultiArray, Bool
 from geometry_msgs.msg import Twist
-# from sensor_msgs.msg import LaserScan
-# from nav_msgs.msg import Odometry
-# from tf import transformations
-# from std_srvs.srv import *
-# import time
-# from move_base_msgs.msg import MoveBaseActionGoal
-from debugpy import listen
+from sensor_msgs.msg import LaserScan
+import time
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
-from os import system
+import os
+import curses
+import sys
+import builtins
 
 # * Program has to do three main things:
 # * - 1) autonomously reach a x,y coordinate inserted by the user
@@ -36,11 +33,11 @@ from os import system
 # * - should not turn left/right if there are obstacles on the
 # * left/right
 # TODO
-# TODO- Main as UI that gets input and sends it to move_base/goal
+# TODO- Main as UI that gets input and sends it to move_base/goal   DONE
 # TODO (implement UI after)
-# TODO- Main that rephrases data form teleop_twist_keyboard
-# TODO  to move_base (remap teleop topic and send it to move_base)
-# TODO- After implementing teleop remap make the function
+# TODO- Main that rephrases data form teleop_twist_keyboard         DONE
+# TODO  to move_base (remap teleop topic and send it to move_base)  DONE
+# TODO- After implementing teleop remap make the function           DONE
 # TODO consider obstacles form laser scanner and MAP
 
 regions = {
@@ -52,68 +49,168 @@ regions = {
 }
 globalVelocity = Twist()
 velocityToSend = Twist()
+
+# globalVelocity.linear.x = 0
+# globalVelocity.linear.y = 0
+# globalVelocity.linear.z = 0
+# globalVelocity.angular.x = 0
+# globalVelocity.angular.y = 0
+# globalVelocity.angular.z = 0
+
 remmapedListner = None
 pubToDrive = None
 pubFromAssisted = None
+# ? Params
+assitanceThreshold = None
+timeoutROS = None
+
+r = None
+menu_options = {
+    1: 'Autonomously reach a x,y coordinate inserted by the user',
+    2: 'Drive the robot with the keyboard',
+    3: 'Drive the robot with assistance to avoid collisions',
+    4: 'Exit',
+    5: "IT'S TIME TO STOP",
+}
+
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
 
 
 def print_menu():
-    system('clear')
+    # TODO drawing UI one at the time?
+    # clear_screen_seq = b''
+
+    # if sys.stdout.isatty():
+    #     curses.setupterm()
+    #     clear_screen_seq = curses.tigetstr('clear')
+    # print("\033c")
+    # print("\033c", end="")
+    os.system('clear')
+    print("\033[96m")
     for key in menu_options.keys():
+        # builtins.print(key, '--', menu_options[key])
         print(key, '--', menu_options[key])
+    print("\033[00m")
+    # time.sleep(0.1)
+
+    # scrn = curses.initscr()
+    # # scrn.erase()
+    # scrn.erase()
+    # scrn.refresh()
+    # curses.endwin()
 
 
 def autoGoalDriveCallback(msg):
-    print("autoGoalDriveCallback running!")
-    print("This is the recievied message : ", msg.data)
+    prYellow("Master:: autoGoalDriveCallback running!")
+    print("\033[93m This is the recievied message : ", msg.data, "\033[00m")
     movebase_client(msg.data[0], msg.data[1])
-    print("Action client returned correctly")
-    menu_options = {
-        1: 'Autonomously reach a x,y coordinate inserted by the user',
-        2: 'Drive the robot with the keyboard',
-        3: 'Drive the robot with assistance to avoid collisions',
-        4: 'Exit',
-        5: 'TIME TO STOP',
-    }
+    prYellow("Action client returned correctly")
     print_menu()
 
 
 def makeDrivingGreatAgain(msg):
+    prLightPurple("\nMaster:: detected teleop input")
     global globalVelocity
-    globalVelocity = msg
+    globalVelocity.linear = msg.linear
+    globalVelocity.angular = msg.angular
+    r.sleep()
+
+    # print("This is the message", msg)
+    # print("This is the globalbelocity", globalVelocity)
 
 
 def manualDriveCallback(msg):
-    global velocityToSend
-    if msg == True:
-        print("manualDriveCallback running!")
-        velocityToSend = globalVelocity
-        pubToDrive.publish(velocityToSend)
+    global globalVelocity
+    if (msg.data == True):
+        prYellow("Master:: manualDriveCallback running!")
+        prCyan("Control of the robot is done through seperate console 'TeleopKey!'")
+    while(msg.data == True):
+        pubToDrive.publish(globalVelocity)
+        r.sleep()
+    prYellow("Master:: manualDriveCallback stopped!")
 
 
 def assistedDriveCallback(msg):
     # TODO algo for avoiding obstacles
-    global velocityToSend
-    if msg == True:
-        print("assistedDriveCallback running!")
-        velocityToSend = globalVelocity
-        pubToDrive.publish(velocityToSend)
+    global regions
+    global globalVelocity
+    if (msg.data == True):
+        prYellow("Master:: assistedDriveCallback running!")
+        prYellow(
+            f"DriveAssist:: threshhold for warnings ->{assitanceThreshold}!")
+        time.sleep(2)
+    while (msg.data == True):
+        print_menu()
+        prYellow("DriveAssist:: drive assist engaged!")
+        # velocityToSend = globalVelocity
+        if (regions['right'] <= 2*assitanceThreshold):
+            print("DriveAssist:: Obstacle detected on the right side!")
+            if (regions['right'] <= assitanceThreshold):
+                print(f"\033[91mDistance-> {regions['right']} \033[00m")
+            else:
+                print(f"\033[95mDistance-> {regions['right']} \033[00m")
+
+        if (regions['fright'] <= 2*assitanceThreshold):
+            print("DriveAssist:: Obstacle detected on the front-right side!")
+            if (regions['fright'] <= assitanceThreshold):
+                print(f"\033[91mDistance-> {regions['fright']} \033[00m")
+            else:
+                print(f"\033[95mDistance-> {regions['fright']} \033[00m")
+
+        if (regions['front'] <= 2*assitanceThreshold):
+            print("DriveAssist:: Obstacle detected on the front side!")
+            if (regions['front'] <= assitanceThreshold):
+                print(f"\033[91mDistance-> {regions['front']} \033[00m")
+            else:
+                print(f"\033[95mDistance-> {regions['front']} \033[00m")
+
+        if (regions['fleft'] <= 2*assitanceThreshold):
+            print("DriveAssist:: Obstacle detected on the front-left side!")
+            if (regions['fleft'] <= assitanceThreshold):
+                print(f"\033[91mDistance-> {regions['fleft']} \033[00m")
+            else:
+                print(f"\033[95mDistance-> {regions['fleft']} \033[00m")
+
+        if (regions['left'] <= 2*assitanceThreshold):
+            print("DriveAssist:: Obstacle detected on the left side!")
+            if (regions['left'] <= assitanceThreshold):
+                print(f"\033[91mDistance-> {regions['left']} \033[00m")
+            else:
+                print(f"\033[95mDistance-> {regions['left']} \033[00m")
+
+        pubToDrive.publish(globalVelocity)
+        # prYellow("Master:: assistedDriveCallback publlished!")
+    prYellow("Master:: assitedDriveCallback stopped!")
+
+    r.sleep()
+
+# Function for finding the closest object on 5 sectors of the scanners
 
 
-# def clbk_laser(msg):
-#     regions = {
-#         'right':  min(min(msg.ranges[0:143]), 10),
-#         'fright': min(min(msg.ranges[144:287]), 10),
-#         'front':  min(min(msg.ranges[288:431]), 10),
-#         'fleft':  min(min(msg.ranges[432:575]), 10),
-#         'left':   min(min(msg.ranges[576:719]), 10),
-#     }
+def clbk_laser(msg):
+    global regions
+    regions = {
+        'right':  min(msg.ranges[0:143]),
+        'fright': min(msg.ranges[144:287]),
+        'front':  min(msg.ranges[288:431]),
+        'fleft':  min(msg.ranges[432:575]),
+        'left':   min(msg.ranges[576:719]),
+    }
+    # print("These are the regions:", regions)
 
 
 def movebase_client(xUI, yUI):
-    print("running move_base_Client")
+    prGreen("Master::running move_base_Client")
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-    client.wait_for_server()
+    client.wait_for_server(rospy.Duration(timeoutROS))
 
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
@@ -123,20 +220,24 @@ def movebase_client(xUI, yUI):
     goal.target_pose.pose.position.y = yUI
 
     client.send_goal(goal)
-    print("Waiting for the action client result")
-    wait = client.wait_for_result()
+    prPurple("Waiting for the action client result")
+    wait = client.wait_for_result(timeoutROS)
     if not wait:
         rospy.logerr("Action server not available!")
         rospy.signal_shutdown("Action server not available!")
     else:
-        print("Finished")
+        prPurple("Finished")
         return client.get_result()
 
 
+# * The main funtion
 if __name__ == '__main__':
     try:
         rospy.init_node('master_Robot')
-        print("MASTER::THIS IS THE TEST AFTER SUBS")
+        r = rospy.Rate(10)  # 10hz
+        # prGreen("Master::THIS IS THE TEST AFTER SUBS")
+        assitanceThreshold = rospy.get_param("/robotAsistanceParam")
+        timeoutROS = rospy.get_param("/pathWaitParam")
 
         rospy.Subscriber("autonomousCoords", Float32MultiArray,
                          autoGoalDriveCallback)
@@ -146,13 +247,15 @@ if __name__ == '__main__':
                          assistedDriveCallback)
         rospy.Subscriber("remmaped_cmd_vel", Twist,
                          makeDrivingGreatAgain)
+        rospy.Subscriber("scan", LaserScan, clbk_laser)
+
         pubToDrive = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
-        result = True
-        print("MASTER::THIS IS THE TEST AFTER SUBS")
+        # result = True
+        # prGreen("MASTER::THIS IS THE TEST AFTER SUBS")
         # result = movebase_client()
-        if result:
-            rospy.loginfo("Goal execution done!")
+        # if result:
+        #     rospy.loginfo("Goal execution done!")
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
